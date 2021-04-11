57c9d85b25d4ddc43271129b0cc868b4cf409f8d
avi <hi@avi.im>
2021-04-11 12:15:38 +0530

    Add FluvioErrorWrapper

--- src/fluvio_go.h
+++ src/fluvio_go.h
@@ -7,11 +7,21 @@ typedef struct FluvioWrapper FluvioWrapper;
 
 typedef struct TopicProducerWrapper TopicProducerWrapper;
 
-struct FluvioWrapper *fluvio_connect(void);
+typedef struct FluvioErrorWrapper {
+  char *msg;
+} FluvioErrorWrapper;
+
+struct FluvioErrorWrapper *custom_error_new(void);
+
+void custom_error_free(struct FluvioErrorWrapper *err_ptr);
+
+struct FluvioWrapper *fluvio_connect(struct FluvioErrorWrapper *err_ptr);
 
 struct TopicProducerWrapper *fluvio_topic_producer(struct FluvioWrapper *ptr,
-                                                   const char *topic_ptr);
+                                                   const char *topic_ptr,
+                                                   struct FluvioErrorWrapper *err_ptr);
 
 void topic_producer_send(struct TopicProducerWrapper *ptr,
                          const char *key_ptr,
-                         const char *value_ptr);
+                         const char *value_ptr,
+                         struct FluvioErrorWrapper *err_ptr);
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,63 @@
 extern crate libc;
 
-use fluvio::{Fluvio, TopicProducer};
+use fluvio::{Fluvio, FluvioError, TopicProducer};
 use fluvio_future::task::run_block_on;
 use libc::c_char;
-use std::ffi::CStr;
+use std::ffi::{CStr, CString};
+
+#[repr(C)]
+pub struct FluvioErrorWrapper {
+    msg: *mut c_char,
+}
+
+impl FluvioErrorWrapper {
+    fn new() -> FluvioErrorWrapper {
+        FluvioErrorWrapper {
+            msg: std::ptr::null_mut(),
+        }
+    }
+}
+
+impl Drop for FluvioErrorWrapper {
+    fn drop(&mut self) {
+        if !self.msg.is_null() {
+            unsafe {
+                CString::from_raw(self.msg);
+            }
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn custom_error_new() -> *mut FluvioErrorWrapper {
+    Box::into_raw(Box::new(FluvioErrorWrapper::new()))
+}
+
+#[no_mangle]
+pub extern "C" fn custom_error_free(err_ptr: *mut FluvioErrorWrapper) {
+    if err_ptr.is_null() {
+        return;
+    }
+    unsafe {
+        Box::from_raw(err_ptr);
+    }
+}
 
 pub struct TopicProducerWrapper {
     inner: TopicProducer,
 }
 
 impl TopicProducerWrapper {
-    fn send<K, V>(&self, key: K, value: V)
+    fn new(inner: TopicProducer) -> Self {
+        TopicProducerWrapper { inner }
+    }
+
+    fn send<K, V>(&self, key: K, value: V) -> Result<(), FluvioError>
     where
         K: Into<Vec<u8>>,
         V: Into<Vec<u8>>,
     {
-        run_block_on(self.inner.send(key, value)).expect("failed to send values");
+        run_block_on(self.inner.send(key, value))
     }
 }
 
@@ -24,28 +66,37 @@ pub struct FluvioWrapper {
 }
 
 impl FluvioWrapper {
-    fn new() -> Self {
-        FluvioWrapper {
-            inner: run_block_on(Fluvio::connect()).expect("failed to connect to fluvio"),
-        }
+    fn new(inner: Fluvio) -> Self {
+        FluvioWrapper { inner }
     }
-    fn topic_producer<S: Into<String>>(&mut self, topic: S) -> TopicProducerWrapper {
-        TopicProducerWrapper {
-            inner: run_block_on(self.inner.topic_producer(topic))
-                .expect("failed to get the topic producer"),
-        }
+    fn connect() -> Result<Fluvio, FluvioError> {
+        run_block_on(Fluvio::connect())
+    }
+    fn topic_producer<S: Into<String>>(&mut self, topic: S) -> Result<TopicProducer, FluvioError> {
+        run_block_on(self.inner.topic_producer(topic))
     }
 }
 
 #[no_mangle]
-pub extern "C" fn fluvio_connect() -> *mut FluvioWrapper {
-    Box::into_raw(Box::new(FluvioWrapper::new()))
+pub extern "C" fn fluvio_connect(err_ptr: *mut FluvioErrorWrapper) -> *mut FluvioWrapper {
+    match FluvioWrapper::connect() {
+        Ok(fluvio_obj) => Box::into_raw(Box::new(FluvioWrapper::new(fluvio_obj))),
+        Err(fluvio_error) => {
+            let err = unsafe {
+                assert!(!err_ptr.is_null());
+                &mut *err_ptr
+            };
+            err.msg = CString::new(fluvio_error.to_string()).unwrap().into_raw();
+            std::ptr::null_mut()
+        }
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn fluvio_topic_producer(
     ptr: *mut FluvioWrapper,
     topic_ptr: *const c_char,
+    err_ptr: *mut FluvioErrorWrapper,
 ) -> *mut TopicProducerWrapper {
     let f = unsafe {
         assert!(!ptr.is_null());
@@ -55,7 +106,17 @@ pub extern "C" fn fluvio_topic_producer(
         assert!(!topic_ptr.is_null());
         CStr::from_ptr(topic_ptr).to_str().unwrap()
     };
-    Box::into_raw(Box::new(f.topic_producer(topic)))
+    match f.topic_producer(topic) {
+        Ok(topic_producer) => Box::into_raw(Box::new(TopicProducerWrapper::new(topic_producer))),
+        Err(fluvio_error) => {
+            let err = unsafe {
+                assert!(!err_ptr.is_null());
+                &mut *err_ptr
+            };
+            err.msg = CString::new(fluvio_error.to_string()).unwrap().into_raw();
+            std::ptr::null_mut()
+        }
+    }
 }
 
 // TODO: change types of key and value to be bytes
@@ -64,6 +125,7 @@ pub extern "C" fn topic_producer_send(
     ptr: *mut TopicProducerWrapper,
     key_ptr: *const c_char,
     value_ptr: *const c_char,
+    err_ptr: *mut FluvioErrorWrapper,
 ) {
     let tp = unsafe {
         assert!(!ptr.is_null());
@@ -77,5 +139,11 @@ pub extern "C" fn topic_producer_send(
         assert!(!value_ptr.is_null());
         CStr::from_ptr(value_ptr).to_str().unwrap()
     };
-    tp.send(key, value);
+    if let Err(fluvio_error) = tp.send(key, value) {
+        let err = unsafe {
+            assert!(!err_ptr.is_null());
+            &mut *err_ptr
+        };
+        err.msg = CString::new(fluvio_error.to_string()).unwrap().into_raw();
+    }
 }
